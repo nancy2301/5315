7990e003e73fe9f1e4668817c917014804cac392
nancy <jainnancy@seattleu.edu>
2019-04-22 00:06:31 -0700

    added json file

--- empdata.js
+++ empdata.js
@@ -13,7 +13,20 @@
       "extension": "8422",
       "joining_date": "2011/07/25",
       "salary": "$150,000"
-    
+    },
+    {
+      "name": "Brianne Tar",
+      "gender": "Female",
+      "streetaddress": "66 Spring Valley Drive",
+      "city": "Buffalo",
+      "state": "New York",
+      "zipcode": "14201",
+      "designation": "Accountant",
+      "office": "New York city",
+      "phonenumber": "345-363-3333",
+      "extension": "4804",
+      "joining_date": "2012/12/02",
+      "salary": "$120,000"
     }
   ]
 }
